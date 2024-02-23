d6c7baf53dd16b9b6f7a11d4de659fd5c6286d65
guilherme.peritto <guilherme.perito@useall.com.br>
2024-02-23 11:26:16 -0300

    ajuste de codigo

--- src/App.tsx
+++ src/App.tsx
@@ -8,10 +8,10 @@ import { GoogleOAuthProvider } from '@react-oauth/google';
 function App() {
   const { userLogged } = useAuth()
   return (
-    <>
+    <GoogleOAuthProvider clientId="187186328176-aj8t1h9vqt0cq8v483o1682jksngk0m9.apps.googleusercontent.com">
       <ToastContainer theme='dark' position='top-right' transition={Slide} />
       <Rotas />
-    </>
+    </GoogleOAuthProvider>
   );
 }
 
--- src/screens/login/login.tsx
+++ src/screens/login/login.tsx
@@ -2,12 +2,11 @@ import React, { useState } from 'react';
 import './login.css';
 import { Link } from 'react-router-dom';
 import Api from '../../infra/api';
-import { Bounce, toast } from 'react-toastify';
 import "react-toastify/dist/ReactToastify.css";
 import { useAuth } from '../../hooks/useAuth';
-import { GoogleLogo } from '@phosphor-icons/react';
-import { GoogleLogin, GoogleOAuthProvider } from '@react-oauth/google';
 import { jwtDecode } from 'jwt-decode';
+import { toast } from 'react-toastify';
+import { GoogleLogin } from '@react-oauth/google';
 
 
 const Login = () => {
@@ -44,78 +43,76 @@ const Login = () => {
 
     return (
         <div className="login-container">
-            <GoogleOAuthProvider clientId="187186328176-aj8t1h9vqt0cq8v483o1682jksngk0m9.apps.googleusercontent.com">
-                <i></i>
-                <i></i>
-                <i></i>
-                <form className='login' onSubmit={logar}>
-                    <h2>Login</h2>
-                    <label className="imputBox" >
-                        <input autoComplete='false' required type="email" placeholder="E-mail:" onChange={(event) => {
-                            if (event) {
-                                setEmail(event.target.value);
-                            }
-                        }} />
-                    </label>
-                    <label className="imputBox">
-                        <input autoComplete='false' required type="password" placeholder="Senha:" onChange={(event) => {
-                            if (event) {
-                                setSenha(event.target.value);
-                            }
-                        }} />
-                    </label>
-                    <label className="login-buttons">
-                        <input type="submit" value="Entrar" />
-                        <div style={{display: "flex"}}>
-                            <GoogleLogin
-                                shape='circle'
-                                type='icon'
-                                onSuccess={async credentialResponse => {
-                                    const token = credentialResponse.credential?.toString(),
-                                        decoded : any = await jwtDecode(token!);
+            <i></i>
+            <i></i>
+            <i></i>
+            <form className='login' onSubmit={logar}>
+                <h2>Login</h2>
+                <label className="imputBox" >
+                    <input autoComplete='false' required type="email" placeholder="E-mail:" onChange={(event) => {
+                        if (event) {
+                            setEmail(event.target.value);
+                        }
+                    }} />
+                </label>
+                <label className="imputBox">
+                    <input autoComplete='false' required type="password" placeholder="Senha:" onChange={(event) => {
+                        if (event) {
+                            setSenha(event.target.value);
+                        }
+                    }} />
+                </label>
+                <label className="login-buttons">
+                    <input type="submit" value="Entrar" />
+                    <div style={{ display: "flex" }}>
+                        <GoogleLogin
+                            shape='circle'
+                            type='icon'
+                            onSuccess={async credentialResponse => {
+                                const token = credentialResponse.credential?.toString(),
+                                    decoded: any = await jwtDecode(token!);
 
-                                    const dto = {
-                                        email: decoded.email,
-                                        senha: decoded.sub,
-                                        nome: decoded.name,
-                                        origem: 1,
-                                        role: "USER"
+                                const dto = {
+                                    email: decoded.email,
+                                    senha: decoded.sub,
+                                    nome: decoded.name,
+                                    origem: 1,
+                                    role: "USER"
+                                }
+
+                                try {
+                                    const { data } = await Api.post('/auth/loginexterno', dto);
+
+                                    if (data) {
+                                        toast.success("Login relizado com sucesso!")
                                     }
 
-                                    try {
-                                        const { data } = await Api.post('/auth/loginexterno', dto);
+                                    await handleSaveUserLogged(data);
 
-                                        if (data) {
-                                            toast.success("Login relizado com sucesso!")
-                                        }
-
-                                        await handleSaveUserLogged(data);
-
-                                    } catch (error: any) {
-                                        if (error.response) {
-                                            toast.error(`${error.response.data}`);
-                                        } else {
-                                            toast.error("Erro ao realizar login!");
-                                        }
+                                } catch (error: any) {
+                                    if (error.response) {
+                                        toast.error(`${error.response.data}`);
+                                    } else {
+                                        toast.error("Erro ao realizar login!");
                                     }
+                                }
 
-                                }}
-                                onError={() => {
-                                    toast.error("Erro ao realizar login!");
-                                }}
-                            />
-                        </div>
-                    </label>
-                    <label className="links">
-                        <Link to='esqueceu'>
-                            <label style={{ cursor: "pointer" }}>Esqueceu a senha?</label>
-                        </Link>
-                        <Link to='criar'>
-                            <label style={{ cursor: "pointer" }}>Criar conta</label>
-                        </Link>
-                    </label>
-                </form>
-            </GoogleOAuthProvider>
+                            }}
+                            onError={() => {
+                                toast.error("Erro ao realizar login!");
+                            }}
+                        />
+                    </div>
+                </label>
+                <label className="links">
+                    <Link to='esqueceu'>
+                        <label style={{ cursor: "pointer" }}>Esqueceu a senha?</label>
+                    </Link>
+                    <Link to='criar'>
+                        <label style={{ cursor: "pointer" }}>Criar conta</label>
+                    </Link>
+                </label>
+            </form>
         </div>
     );
 }
