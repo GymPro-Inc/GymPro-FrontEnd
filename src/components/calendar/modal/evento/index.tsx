46780c8d5c0a08b5a050c3c9b901c7cd1e3bbc4a
guilherme.peritto <guilherme.perito@useall.com.br>
2024-02-29 17:35:28 -0300

    ...

--- src/components/calendar/modal/evento/index.tsx
+++ src/components/calendar/modal/evento/index.tsx
@@ -1,7 +1,3 @@
-import React from 'react';
-import { format } from 'date-fns';
-import { pt } from 'date-fns/locale';
-import Draggable from 'react-draggable';
 import './evento.css';
 import { X } from '@phosphor-icons/react';
 
@@ -12,60 +8,64 @@ interface EventoProps {
 
 const Evento = ({ date, onClose }: EventoProps) => {
     return (
-        <Draggable handle=".barra">
             <div className="modalEvento">
                 <div className="barra">
                     <X className="botao-fechar" onClick={onClose} />
                 </div>
                 <div className="conteudo">
                     <div className="formulario">
-                        <div className='campoTexto'>
-                            <label>Titulo: </label>
-                            <input required type="text" placeholder="email@exemplo.com" onChange={(event) => {
-                                if (event) {
-                                    // setEmail(event.target.value);
-                                }
-                            }} />
+                        <div className='colunas'>
+                            <div className='campoTexto'>
+                                <label>Titulo: </label>
+                                <input required type="text" placeholder="email@exemplo.com" onChange={(event) => {
+                                    if (event) {
+                                        // setEmail(event.target.value);
+                                    }
+                                }} />
+
+                            </div>
+                            <div className='campoTexto'>
+                                <label>Titulo: </label>
+                                <input required type="text" placeholder="email@exemplo.com" onChange={(event) => {
+                                    if (event) {
+                                        // setEmail(event.target.value);
+                                    }
+                                }} />
+                            </div>
                         </div>
-                        <div className='campoTexto'>
-                            <label>Titulo: </label>
-                            <input required type="text" placeholder="email@exemplo.com" onChange={(event) => {
-                                if (event) {
-                                    // setEmail(event.target.value);
-                                }
-                            }} />
+                        <div className='colunas'>
+                            <div className='campoTexto'>
+                                <label>Titulo: </label>
+                                <input required type="text" placeholder="email@exemplo.com" onChange={(event) => {
+                                    if (event) {
+                                        // setEmail(event.target.value);
+                                    }
+                                }} />
+                            </div>
+                            <div className='campoTexto'>
+                                <label>Titulo: </label>
+                                <input required type="text" placeholder="email@exemplo.com" onChange={(event) => {
+                                    if (event) {
+                                        // setEmail(event.target.value);
+                                    }
+                                }} />
+                            </div>
                         </div>
-                        <div className='campoTexto'>
-                            <label>Titulo: </label>
-                            <input required type="text" placeholder="email@exemplo.com" onChange={(event) => {
-                                if (event) {
-                                    // setEmail(event.target.value);
-                                }
-                            }} />
-                        </div>
-                        <div className='campoTexto'>
-                            <label>Titulo: </label>
-                            <input required type="text" placeholder="email@exemplo.com" onChange={(event) => {
-                                if (event) {
-                                    // setEmail(event.target.value);
-                                }
-                            }} />
-                        </div>
-                        <div className='campoTexto'>
-                            <label>Titulo: </label>
-                            <input required type="text" placeholder="email@exemplo.com" onChange={(event) => {
-                                if (event) {
-                                    // setEmail(event.target.value);
-                                }
-                            }} />
-                            
+                        <div className='colunas'>
+                            <div className='campoTexto'>
+                                <label>Titulo: </label>
+                                <input required type="text" placeholder="email@exemplo.com" onChange={(event) => {
+                                    if (event) {
+                                        // setEmail(event.target.value);
+                                    }
+                                }} />
+                            </div>
                         </div>
                     </div>
                     <div className="eventos">
                     </div>
                 </div>
             </div>
-        </Draggable >
     );
 }
 
