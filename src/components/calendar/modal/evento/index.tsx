b0a62960bace507069ec9cdc8433b21770e78a5e
GuilhermePeritto <perittoguilherme@gmail.com>
2024-03-13 21:17:42 -0300

    feat/criando modal

--- src/components/calendar/modal/evento/index.tsx
+++ src/components/calendar/modal/evento/index.tsx
@@ -1,15 +1,45 @@
 import { format } from 'date-fns';
 import { X } from '@phosphor-icons/react';
 import { pt } from 'date-fns/locale';
+import { Dialog, DialogClose, DialogContent, DialogDescription, DialogFooter, DialogHeader, DialogPortal, DialogTitle } from '@/components/ui/dialog';
+import { Button } from '@/components/ui/button';
+import { Input } from '@/components/ui/input';
+import { CopyIcon } from '@radix-ui/react-icons';
+import { Label } from '@radix-ui/react-label';
+import { DialogTrigger } from '@radix-ui/react-dialog';
 
 interface EventoProps {
     date: Date;
     onClose: () => void;
 }
 
-const Evento = ({  }: EventoProps) => {
+const Evento = ({ }: EventoProps) => {
     return (
-    <></>
+        <DialogContent onInteractOutside={(evento) => evento.preventDefault()} className="sm:max-w-[60%] h-[60%]">
+            <DialogHeader>
+                <DialogTitle>Edit profile</DialogTitle>
+                <DialogDescription>
+                    Make changes to your profile here. Click save when you're done.
+                </DialogDescription>
+            </DialogHeader>
+            <div className="grid gap-4 py-4">
+                <div className="grid grid-cols-4 items-center gap-4">
+                    <Label htmlFor="name" className="text-right">
+                        Name
+                    </Label>
+                    <Input id="name" value="Pedro Duarte" className="col-span-3" />
+                </div>
+                <div className="grid grid-cols-4 items-center gap-4">
+                    <Label htmlFor="username" className="text-right">
+                        Username
+                    </Label>
+                    <Input id="username" value="@peduarte" className="col-span-3" />
+                </div>
+            </div>
+            <DialogFooter>
+                <Button type="submit">Save changes</Button>
+            </DialogFooter>
+        </DialogContent>
     );
 }
 
--- src/components/calendar/visualizacao/mensal/index.tsx
+++ src/components/calendar/visualizacao/mensal/index.tsx
@@ -2,6 +2,7 @@ import React, { useState } from 'react';
 import { format, eachDayOfInterval, isSameMonth, isSameWeek, isToday } from 'date-fns';
 import { pt } from 'date-fns/locale';
 import Evento from '../../modal/evento';
+import { Dialog, DialogTrigger } from '@/components/ui/dialog';
 
 interface MensalProps {
   semanasDoMes: Date[];
@@ -16,29 +17,33 @@ const Mensal = ({ semanasDoMes, currentDate }: MensalProps) => {
   };
 
   return (
-    <div className="grid grid-cols-1 items-center h-4/5 gap-5">
-      {semanasDoMes.map((semana, indiceSemana) => (
-        <div key={indiceSemana} className="grid grid-cols-7 gap-5">
-          {eachDayOfInterval({
-            start: semana,
-            end: new Date(semana.getFullYear(), semana.getMonth(), semana.getDate() + 6),
-          }).map((dia, indiceDia) => (
-            <div
-              key={indiceDia}
-              onClick={() => handleDayClick(dia)}
-              className={`border p-3 text-center h-full flex flex-col justify-between cursor-pointer
+    <Dialog>
+      <div className="grid grid-cols-1 items-center h-4/5 gap-5">
+        {semanasDoMes.map((semana, indiceSemana) => (
+          <div key={indiceSemana} className="grid grid-cols-7 gap-5">
+            {eachDayOfInterval({
+              start: semana,
+              end: new Date(semana.getFullYear(), semana.getMonth(), semana.getDate() + 6),
+            }).map((dia, indiceDia) => (
+              <DialogTrigger>
+                <div
+                  key={indiceDia}
+                  onClick={() => handleDayClick(dia)}
+                  className={`border p-3 text-center h-full flex flex-col justify-between cursor-pointer
                 ${isSameMonth(dia, currentDate) ? ' hover:border-200 hover:text-200 border-2' : 'text-gray-300 border-gray-300 font-bold text-gray-300'}
                 ${isToday(dia) ? 'bg-200 text-white hover:bg-white' : 'bg-white'}
                 rounded-2xl text-black gap-[100px] `}
-            >
-              <span className="font-bold">{format(dia, 'dd')}</span>
-              <span className="font-bold">{format(dia, 'EEEE', { locale: pt })}</span>
-            </div>
-          ))}
-        </div>
-      ))}
-      {selectedDate && <Evento date={selectedDate} onClose={() => setSelectedDate(null)} />}
-    </div>
+                >
+                  <span className="font-bold">{format(dia, 'dd')}</span>
+                  <span className="font-bold">{format(dia, 'EEEE', { locale: pt })}</span>
+                </div>
+              </DialogTrigger>
+            ))}
+          </div>
+        ))}
+        {selectedDate && <Evento date={selectedDate} onClose={() => setSelectedDate(null)} />}
+      </div>
+    </Dialog>
   );
 };
 
